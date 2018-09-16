8fe16655ab07abf73fa0d971e74ab0f93529a561
mosin <ryoctrl@gmail.com>
2018-09-17 00:43:49 +0900

    added polugin toml files

--- envInitialize.sh
+++ envInitialize.sh
@@ -2,3 +2,5 @@
 DOTFILE_DIR=$(cd $(dirname "${BASH_SOURCE:-$0}"); pwd)
 
 ln -sf $DOTFILE_DIR/.vimrc ~/.vimrc
+ln -sf $DOTFILE_DIR/plugins.toml ~/.vim/plugins.toml
+ln -sf $DOTFILE_DIR/plugins_lazy.toml ~/.vim/plugins_lazy.toml
