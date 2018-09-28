aa304c4e9e54c5acf9cb0b6388f0120b09f963a4
mosin <mosin@mosin.local>
2018-09-28 16:26:34 +0900

    add gitconfig

--- envInitialize.sh
+++ envInitialize.sh
@@ -18,4 +18,5 @@ DOTFILE_DIR=$(cd $(dirname "${BASH_SOURCE:-$0}"); pwd)
 ln -sf $DOTFILE_DIR/.vimrc ~/.vimrc
 ln -sf $DOTFILE_DIR/plugins.toml ~/.vim/plugins.toml
 ln -sf $DOTFILE_DIR/plugins_lazy.toml ~/.vim/plugins_lazy.toml
-ln -sf $DOTFILE_DIR/.bash_profile ~/.bash_proflie
+#ln -sf $DOTFILE_DIR/.bash_profile ~/.bash_proflie
+ln -sf $DOTFILE_DIR/.gitconfig ~/.gitconfig
